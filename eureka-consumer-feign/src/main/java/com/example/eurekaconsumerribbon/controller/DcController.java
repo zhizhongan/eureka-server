6b1f17ad807f5c22017951b6c320905770aefe60
Sam <785671128@qq.com>
2019-09-04 00:03:24 +0800

    加入了Feign

---
+++ eureka-consumer-feign/src/main/java/com/example/eurekaconsumerribbon/controller/DcController.java
@@ -0,0 +1,25 @@
+package com.example.eurekaconsumerribbon.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.loadbalancer.LoadBalancerClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+/**
+ * @author Sam
+ * @date 2019/9/2 15:04
+ */
+@RestController
+public class DcController {
+
+    @Autowired
+    DcClient dcClient;
+
+    @GetMapping("/consumer")
+    public String dc() {
+        return dcClient.consumer();
+    }
+
+}
