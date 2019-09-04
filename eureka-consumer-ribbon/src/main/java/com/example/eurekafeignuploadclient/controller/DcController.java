2d9b9949eca8c94a722abf64d9da099464562ae4
zhizhongan <785671128@qq.com>
2019-09-04 18:15:51 +0800

    加入了文件上传部分的代码

---
+++ eureka-consumer-ribbon/src/main/java/com/example/eurekafeignuploadclient/controller/DcController.java
@@ -0,0 +1,23 @@
+package com.example.eurekafeignuploadclient.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+/**
+ * @author Sam
+ * @date 2019/9/2 15:04
+ */
+@RestController
+public class DcController {
+
+    @Autowired
+    RestTemplate restTemplate;
+
+    @GetMapping("/consumer")
+    public String dc() {
+        return restTemplate.getForObject("http://eureka-client/dc", String.class);
+    }
+
+}
